fd464f9a6724d5a6d6adb3ab82ffe9a5e772b218
zhaofan <hjzhaofan@163.com>
2019-08-12 09:43:14 +0800

    添加测试代码

--- yamlConfig_test.go
+++ yamlConfig_test.go
@@ -1,6 +1,8 @@
 package yamlConfig
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestConfigEngine_Load(t *testing.T) {
 	config := &ConfigEngine{}
@@ -11,4 +13,85 @@ func TestConfigEngine_Load(t *testing.T) {
 	t.Logf("%v", config)
 	res := config.Get("mysql.username")
 	t.Log(res)
+}
+
+func TestConfigEngine_GetString(t *testing.T) {
+	config := &ConfigEngine{}
+	err := config.Load("config.yaml")
+	if err != nil {
+		t.Fatalf("config load error:%v",err)
+	}
+	t.Logf("%v",config)
+	res := config.GetString("app")
+	t.Log(res)
+}
+
+func TestConfigEngine_GetBool(t *testing.T) {
+	config := &ConfigEngine{}
+	err := config.Load("config.yaml")
+	if err != nil {
+		t.Fatalf("config load error:%v",err)
+	}
+	t.Logf("%v",config)
+	res := config.GetString("enable")
+	t.Log(res)
+}
+
+func TestConfigEngine_GetInt(t *testing.T) {
+	config := &ConfigEngine{}
+	err := config.Load("config.yaml")
+	if err != nil {
+		t.Fatalf("config load error:%v",err)
+	}
+	t.Logf("%v",config)
+	res := config.GetInt("port")
+	t.Log(res)
+}
+
+func TestConfigEngine_GetFloat64(t *testing.T) {
+	config := &ConfigEngine{}
+	err := config.Load("config.yaml")
+	if err != nil {
+		t.Fatalf("config load error:%v",err)
+	}
+	t.Logf("%v",config)
+	res := config.GetString("timeout")
+	t.Log(res)
+}
+
+
+// 关于获取结构体的时候需要注意的是，结构体的字段必须和配置文件中的字段名称是一致的
+type MysqlConfig struct {
+	UserName string
+	PassWord string
+	DataBase string
+	Host string
+	Port int
+	TimeOut float64
+}
+
+func TestConfigEngine_GetStruct(t *testing.T) {
+	config := &ConfigEngine{}
+	err := config.Load("config.yaml")
+	if err != nil {
+		t.Fatalf("config load error:%v",err)
+	}
+	t.Logf("%v",config)
+	res := config.GetStruct("mysql", &MysqlConfig{})
+	t.Log(res)
+}
+
+func TestConfigEngine_Get(t *testing.T) {
+	config := &ConfigEngine{}
+	err := config.Load("config.yaml")
+	if err != nil {
+		t.Fatalf("config load error:%v",err)
+	}
+	t.Logf("%v",config)
+	res := config.Get("hotwords")
+	t.Log(res)
+}
+
+type Config struct {
+
 }
